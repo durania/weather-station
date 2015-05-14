80f5bad105f195e4fdb65cebe06e286aead83670
Daniele Urania <raytracer1975@gmail.com>
2015-05-14 22:41:12 +0100

    add responsive layout

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,16 +3,18 @@
 angular.module('weatherStation')
     .controller('MainCtrl', ['$scope', 'openWeather', function($scope, openWeather) {
         $scope.list = [];
+        $scope.selected = null;
         openWeather.all()
             .then(function(data) {
-                $scope.list = data;   
+                $scope.list = data;
             });
         $scope.showDetails = function(selected) {
+            $scope.selected = selected;
             $scope.list.forEach(function(item) {
                 item.isActive = false;
                 if (item.city === selected.city) {
                     item.isActive = true;
-                };
+                }
             });
         };
     }]);
